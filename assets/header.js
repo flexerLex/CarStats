9af98c4a4d249ddb4366c7b7ca40fdc5a0322919
WANZL\olepyl <oleksi.pylypenko@wanzl.com>
2025-11-13 14:06:00 +0100

    Implement dynamic header loading

---
+++ assets/header.js
@@ -0,0 +1,6 @@
+fetch('templates/header.html')
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById('header-placeholder').innerHTML = html;
+    })
+    .catch((error) => console.error('Error loading header:', error));
