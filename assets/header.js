e41abd4423df6d9663790c4485350c26fd304601
HuiqinQiu <huiqinqiu999@gmail.com>
2025-12-10 13:06:53 +0100

    Den Fehler beheben, dass die aktuelle Seite im Navbar nicht markiert wird.

--- assets/header.js
+++ assets/header.js
@@ -1,17 +1,38 @@
 fetch('../../Templates/header.html')
     .then((response) => response.text())
     .then((html) => {
+
         document.getElementById('header-placeholder').innerHTML = html;
 
-        // Dynamic highlighting of active link
-        const currentPage = window.location.pathname.split('/').pop();
+        const currentPath = window.location.pathname; 
+        const currentFileName = currentPath.split('/').pop(); 
+        
         document.querySelectorAll('.site-header__nav-link').forEach(link => {
-            const linkPage = link.getAttribute('href');
-            if (linkPage === currentPage) {
+            const linkHref = link.getAttribute('href'); 
+          
+            const linkFileName = linkHref.split('/').pop();
+
+            if (linkFileName === currentFileName && currentFileName !== "") {
                 link.classList.add('active');
             } else {
                 link.classList.remove('active');
             }
         });
+        
+        const navToggle = document.getElementById('site-header__nav-toggle');
+        const overlay = document.querySelector('.site-header__overlay');
+        const navLinks = document.querySelectorAll('.site-header__nav-link');
+
+        if (overlay) {
+            overlay.addEventListener('click', () => {
+                navToggle.checked = false;
+            });
+        }
+        navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                navToggle.checked = false;
+            });
+        });
+
     })
     .catch((error) => console.error('Error loading header:', error));
